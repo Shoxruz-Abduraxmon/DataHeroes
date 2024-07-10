ceb167396fa3b055f44bb645d265de8ab545834c
Shoxruz Abduraxmonov <126638946+Shoxruz-Abduraxmon@users.noreply.github.com>
2024-07-10 14:24:26 +0500

    Update index.js

--- index.js
+++ index.js
@@ -11,24 +11,26 @@ const PORT = process.env.PORT || 3001;
 
 try {
   execSync("mkdir -p ~/.postgresql");
-  execSync('wget "https://storage.yandexcloud.net/cloud-certs/CA.pem" --output-document ~/.postgresql/root.crt');
+  execSync(
+    'wget "https://storage.yandexcloud.net/cloud-certs/CA.pem" --output-document ~/.postgresql/root.crt',
+  );
   execSync("chmod 0600 ~/.postgresql/root.crt");
 } catch (error) {
-  console.error("Error sertificat ", error);
+  console.error("Error downloading certificate:", error);
 }
 
 const config = {
-  connectionString: "postgres://candidate:62I8anq3cFq5GYh2u4Lh@rc1b-r21uoagjy1t7k77h.mdb.yandexcloud.net:6432/db1",
+  connectionString:
+    "postgres://candidate:62I8anq3cFq5GYh2u4Lh@rc1b-r21uoagjy1t7k77h.mdb.yandexcloud.net:6432/db1",
   ssl: {
     rejectUnauthorized: true,
     ca: fs.readFileSync("/home/runner/.postgresql/root.crt").toString(),
   },
 };
-
 const conn = new pg.Client(config);
 
-const createTableQuery = `
-  CREATE TABLE IF NOT EXISTS my_github_username (
+const createTab = `
+  CREATE TABLE IF NOT EXISTS shoxruzabduraxmon (
       id SERIAL PRIMARY KEY,
       name VARCHAR(255) NOT NULL,
       status VARCHAR(50),
@@ -42,19 +44,21 @@ const createTableQuery = `
 `;
 
 const insert = `
-INSERT INTO my_github_username (name, status, species, gender, origin, location, image, episode_count)
+INSERT INTO shoxruzabduraxmon (name, status, species, gender, origin, location, image, episode_count)
 VALUES ($1, $2, $3, $4, $5, $6, $7, $8);
 `;
 
-async function fetchAndStoreCharacters() {
+async function fetch() {
   try {
     await conn.connect();
-    await conn.query(createTableQuery);
+    await conn.query(createTab);
     let page = 1;
     let totalPages = 1;
 
     while (page <= totalPages) {
-      const response = await axios.get(`https://rickandmortyapi.com/api/character/?page=${page}`);
+      const response = await axios.get(
+        `https://rickandmortyapi.com/api/character/?page=${page}`,
+      );
       const characters = response.data.results;
       totalPages = response.data.info.pages;
 
@@ -73,15 +77,15 @@ async function fetchAndStoreCharacters() {
       }
       page++;
     }
-    console.log("Inserted");
+    console.log("Data inserted");
   } catch (e) {
-    console.error(e);
+    console.error("Error:", e);
   } finally {
     await conn.end();
   }
 }
 
-fetchAndStoreCharacters();
+fetch();
 
 app.use(express.static("public"));
 
@@ -89,14 +93,13 @@ app.get("/api/characters", async (req, res) => {
   try {
     const conn = new pg.Client(config);
     await conn.connect();
-    const result = await conn.query("SELECT * FROM my_github_username");
+    const result = await conn.query("SELECT * FROM shoxruzabduraxmon");
     res.json(result.rows);
     await conn.end();
   } catch (e) {
-    console.log("Error" + e);
+    console.error("Error fetching data:", e);
   }
 });
-
 app.listen(PORT, () => {
-  console.log(`localhost: ` + PORT);
+  console.log(`localhost: ` +PORT);
 });
